74cfc4500c4ec4482f128f539fbffb4a2ed4b686
Arthur Cassim <arthurcassim.dev@gmail.com>
2026-05-08 20:24:47 -0300

    Create ex06.cs

---
+++ aula-02/ex06.cs
@@ -0,0 +1,9 @@
+using System;
+
+class Program {
+    static void Main() {
+        double celsius = double.Parse(Console.ReadLine());
+        double fahrenheit = (celsius * 9 / 5) + 32;
+        Console.WriteLine(fahrenheit);
+    }
+}
