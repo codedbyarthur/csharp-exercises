3d9b05a7ed4c1f5009a86de83f13219e2ea21284
Arthur Cassim <arthurcassim.dev@gmail.com>
2026-05-08 20:23:31 -0300

    Create ex02.cs

---
+++ aula-01/ex02.cs
@@ -0,0 +1,9 @@
+using System;
+
+class Program {
+    static void Main() {
+        Console.Write("Digite seu nome: ");
+        string nome = Console.ReadLine();
+        Console.WriteLine("Olá, " + nome);
+    }
+}
