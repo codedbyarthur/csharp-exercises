5522922c48f1c3560de302963df61b80a46782d9
Arthur Cassim <arthurcassim.dev@gmail.com>
2026-05-08 20:24:32 -0300

    Create ex05.cs

---
+++ aula-02/ex05.cs
@@ -0,0 +1,10 @@
+using System;
+
+class Program {
+    static void Main() {
+        double baseR = double.Parse(Console.ReadLine());
+        double altura = double.Parse(Console.ReadLine());
+        double area = baseR * altura;
+        Console.WriteLine(area);
+    }
+}
